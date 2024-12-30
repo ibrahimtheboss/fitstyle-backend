1bed705753e61d6fb433bbe69bd3dc47692488d2
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-12-30 21:58:51 +0400

    fix 1

--- server.js
+++ server.js
@@ -20,11 +20,12 @@ app.use('/api/products', productRoutes);
 
 // For local development
 if (process.env.NODE_ENV !== 'production') {
+  // Local development (Express app)
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+} else {
+  // Serverless deployment (Vercel)
+  module.exports.handler = serverless(app); // Export as a serverless function
 }
-
-// Export the serverless handler for serverless platforms like Vercel
-module.exports.handler = serverless(app); // Export as a serverless function
