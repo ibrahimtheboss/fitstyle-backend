77b2277b2f796f9c94e85192261ad9c82fdd2ec8
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-12-30 12:27:34 +0400

    fix

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://fitstyle:123@fitstyle.5plqw.mongodb.net/?retryWrites=true&w=majority&appName=fitstyle', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://fitstyle:123@fitstyle.5plqw.mongodb.net/?retryWrites=true&w=majority&appName=fitstyle');
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err.message);
